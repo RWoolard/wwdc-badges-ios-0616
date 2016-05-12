19e8dd4ed02d58a024710fae9391e37989fa0871
RWoolard <r.woolard456@gmail.com>
2016-05-11 22:09:48 -0400

    Conference Badges

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -14,6 +14,24 @@
      
      */
     
+    // Conference Speakers
+    
+    NSArray *conferenceSpeakers = @[@"Anita Borg", @"Alan Kay", @"Ada Levelace", @"Aaron Swartz", @"Alan Turing", @"Michael Faraday", @"Grace Hopper", @"Charles Babbage"];
+    
+    for (NSUInteger speaker = 0; speaker < 8; speaker++) {
+        
+        NSLog(@"Hello, my name is %@.", conferenceSpeakers[speaker] );
+    }
+    
+    // Advanced Conference Speakers
+    
+    NSArray *speakerFirst = @[@"Anita", @"Alan", @"Ada", @"Aaron", @"Alan", @"Michael", @"Grace", @"Charles"];
+    NSArray *speakerLast = @[@"Borg", @"Kay", @"Levelace", @"Swartz", @"Turing", @"Faraday", @"Hopper", @"Babbage"];
+    for (NSUInteger firstLast = 0; firstLast < 8; firstLast++) {
+        NSLog(@"Hello, my name is %@ %@.", speakerFirst[firstLast], speakerLast[firstLast]);
+    }
+    
+    
     // do not alter
     return YES;  //
     ///////////////
